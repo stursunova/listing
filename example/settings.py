db165d40ea486f15320bb422e20ec68d58f49ebc
stursunova <101735543+stursunova@users.noreply.github.com>
2022-11-29 00:38:42 +0100

    Update settings.py

--- example/settings.py
+++ example/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q8#dbjl&v1l0gl^h!df2*fbow0r-)p6xgi*t7bx3f!(+gx!o-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [educative-listing.azurewebsites.net]
 
 
 # Application definition
